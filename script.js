3b45f802ed94709d6be72ef731b83edc6e3cbf64
Tahasin Raza Khan <tahasinrazakhan7@gmail.com>
2022-01-29 18:40:14 +0530

    day two

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
  let value1 = document.getElementById('value1');
 let value2 = document.getElementById('value2');
 let value3 = document.getElementById('value3');
+let inspeed = document.getElementById('inspeed');
+
 
 let values =[
 '😃','😋','😐','🤐','😮','🤑','🥶','🤖','😡','😷','🙄'
@@ -11,10 +13,23 @@ function getRandomValue(){
     return  values[Math.floor(Math.random()*11)];
 
 }
-
-setInterval(() => {
+let animationid;
+function updateanimation(newspeed) {
+  if(animationid) clearInterval(animationid);
+  
+animationid=setInterval(() => {
    value1.innerText=getRandomValue()  ;
    value2.innerText=getRandomValue()  ;
    value3.innerText=getRandomValue() ; 
     
-}, 300);
+}, 1000/newspeed);
+
+}
+
+inspeed.onchange= function (ev){
+
+//   console.log('value change',ev.target.value);
+  document.documentElement.style.setProperty('--speed',ev.target.value);
+  updateanimation(ev.target.value);
+
+};
